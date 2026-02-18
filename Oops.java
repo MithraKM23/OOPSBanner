2c6ec21042dde942c7ab5ee12b4da382d61c238e
MithraKM23 <mithrakm2005@gmail.com>
2026-02-18 22:55:46 +0530

    Refactor Banner Logic into Functions

--- Oops.java
+++ Oops.java
@@ -1,22 +1,64 @@
 /**
+*OOPSBannerApp UC6 - OOPS Banner Application (Use Case 6)
+*
+*This use case extends UC5 by implementing a modular approach to generate each letter's pattern through
+*dedicated methods.
 *@author Developer
-*@version 5
+*@version 6
 */
 class Oops{
-      public static void main(String[] args){
-		 String lines[]={
-        String.join("  ", "   ***   ", "   ***   ", "****** ", "   ***** "),
-         String.join("  ", " **   **", "  **   **", "  **   **", " **"),
-         String.join("  ", "**     **", "**     **", " **    **", "**"),
-        String.join("  ", "**     **", "**     **", "  **   **", " **"),
-         String.join("  ", "**     **", "**     **", " ****** ", "   ****"),
-         String.join("  ", "**     **", "**     **", " **     ", "      **"),
-         String.join("  ", "**     **", "**     **", " **     ", "       **"),
-        String.join("  ", " **   **", "  **   **", "  **     ", "      **"),
-        String.join("  ", "   ***   ", "   ***   ", " **     ", "  ***** ")
-	    };
-	    for(String line: lines){
-         System.out.println(line);
-        }
+	//Method to create O pattern
+	public static String[] getopattern(){
+			return new String[]{
+				"   ***   ",
+				" **   ** ",
+				"**     **",
+				"**     **",
+				"**     **",
+				"**     **",
+				"**     **",
+				" **   ** ",
+				"   ***   "
+            };				
+		  }
+	//Method to create P pattern
+	public static String[] getppattern(){
+		return new String[]{
+			"****** ",
+			"**   ** ",
+			"**    **",
+			"**   **",
+			"****** ",
+			"**     ",
+			"**     ",
+			"**     ",
+			"**     "
+		};
+	}
+	//Method to create S pattern
+	public static String[] getspattern(){
+		return new String[]{
+			"   ***** ",
+			" **",
+			"**",
+			" **",
+			"   ****",
+			"      **",
+			"       **",
+			"      **",
+			"  ***** "
+		};
+	}
+		
+    public static void main(String[] args){
+		  
+		 String opattern[]=getopattern();
+		 String ppattern[]=getppattern();
+		 String spattern[]=getspattern();
+		 for(int i=0;i<opattern.length;i++){
+			 System.out.println(opattern[i]+"  "+opattern[i]+"  "+ppattern[i]+"  "+spattern[i]);
+		 }
+		
+		
 }
 }
