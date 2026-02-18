40e6e1e7355ecfa2bd6fd1534f8230dd6a860f98
MithraKM23 <mithrakm2005@gmail.com>
2026-02-18 15:09:47 +0530

    Coded to render OOPS as Banner using Print Statements

---
+++ Oops.java
@@ -0,0 +1,18 @@
+/*
+* @author Developer
+* @version 2.0
+**/
+class OopsBanner{
+	public static void main(String[] args){
+		System.out.println("   ***   " + " " + "   ***   " + " " + " ******   " + "  ***** ");
+		System.out.println(" **   **" + " " + "  **   **" + " " + "  **   **" + " " +  " **");
+		System.out.println("**     **" + " " + "**     **" + " " + " **    **" + " " + "**");
+		System.out.println("**     **" + " " + "**     **" + "  **   **" + " " + " " + " **");
+		System.out.println("**     **" + " " + "**     **" + " " + " ****** " + " " + "   ****");
+		System.out.println("**     **" + " " + "**     **" + " " + " **     " + " " + "      **");
+		System.out.println("**     **" + " " + "**     **" + " " + " **     " + " " + "       **");
+		System.out.println(" **   **" + " " + "  **   **" + " " + "  **     " + " " + "      **");
+		System.out.println("   ***   " + " " + "   ***   " + " " + " **     " + "  ***** ");
+	}
+}
+
